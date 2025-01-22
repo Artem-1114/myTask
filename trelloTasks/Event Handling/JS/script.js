32457943fe23935d20a2b4c8ecd2e43196d3986a
Artem-1114 <artem.ishe1114@gmail.com>
2025-01-23 00:38:21 +0200

    this is still a raw version, I still want to paint it with helpinputField and I did not work today in codewars because I have been solving this Task for 5 hours

---
+++ trelloTasks/Event Handling/JS/script.js
@@ -0,0 +1,70 @@
+function validateName(name) {
+    const regex = /^[a-zA-Zа-яА-ЯёЁЇїІіЄєҐґ\s]+$/;
+    return regex.test(name.trim());
+}
+function validateAndShow() {
+    const input = document.getElementById("name");
+    const error = document.getElementById("error-name");
+
+    if (!validateName(input.value)) {
+        input.classList.add("border");
+        error.classList.add("show");
+    } else {
+        input.classList.remove("border");
+        error.classList.remove("show");
+    }
+}
+document.getElementById("name").addEventListener("blur", validateAndShow);
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email.trim());
+}
+
+function validateAndShowEmail() {
+    const input = document.getElementById("email");
+    const error = document.getElementById("email-example");
+
+    if (!validateEmail(input.value)) {
+        input.classList.add("border");
+        error.classList.remove("show");
+    } else {
+        input.classList.remove("border");
+        error.classList.add("show");
+    }
+}
+
+document.getElementById("email").addEventListener("blur", validateAndShowEmail);
+
+function validatePassword(password) {
+    const passwordRegex = /^[a-zA-Z0-9]+$/;
+    return passwordRegex.test(password.trim());
+}
+
+function validateAndShowPassword() {
+    const input = document.getElementById("password");
+    const error = document.getElementById("error-password");
+
+    if (!validatePassword(input.value)) {
+        input.classList.add("border");
+        error.classList.add("show");
+    } else {
+        input.classList.remove("border");
+        error.classList.remove("show");
+    }
+}
+
+
+document.getElementById("password").addEventListener("blur", validateAndShowPassword);
+
+let button = document.getElementById("submit");
+button.addEventListener("click", function () {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (validateName(name) && validateEmail(email) && validatePassword(password)) {
+        alert("Ви успішно зареєструвались");
+    } else {
+        alert("Помилка при реєстрації");
+    }
+});
